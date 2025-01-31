9f9d83ec55d154aa6c3bfe39598fb0fe4c572f07
MatheusFranceschini <masuzena@gmail.com>
2025-01-31 19:10:06 -0300

    Layout and constraint setup

--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var soundButtonCollection: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLayout()
     }
 
+    func configureLayout() {
+        let borderColor: CGColor = CGColor(red: 48/255, green: 48/255, blue: 48/255, alpha: 1)
+        
+        for button in soundButtonCollection {
+            button.layer.cornerRadius = 16
+            button.layer.borderWidth = 2
+            button.layer.borderColor = borderColor
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
 
 }
 
